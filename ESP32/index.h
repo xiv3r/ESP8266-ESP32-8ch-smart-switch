f17a487e67884fb51dcae0d02e6f49e18fc4014b
𝑹𝒂𝒇𝒇 𝑨𝒍𝒅𝒔 <117867334+xiv3r@users.noreply.github.com>
2026-04-09 17:57:35 +0800

    Fix

--- ESP32/index.h
+++ ESP32/index.h
@@ -4,7 +4,7 @@ const char MAIN_page[] PROGMEM = R"=====(
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ESP8266 Relay Control</title>
+    <title>ESP32 Relay Control</title>
     <style>
         *{box-sizing:border-box;margin:0;padding:0}
         body{

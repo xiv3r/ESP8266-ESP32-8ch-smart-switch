20f3066e0ab689aeadafb4a628dbfa1af1932735
𝑹𝒂𝒇𝒇 𝑨𝒍𝒅𝒔 <117867334+xiv3r@users.noreply.github.com>
2026-04-10 12:43:02 +0800

    Fix

--- ESP8266/ESP8266_8CH_Smart_Switch.ino
+++ ESP8266/ESP8266_8CH_Smart_Switch.ino
@@ -1,13 +1,14 @@
 // REQUIRES the following Arduino libraries:
-// github.com/xiv3r
-
 #include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include "index.h" 
+#include <DNSServer.h>
+#include "index.h"
 
 #define EEPROM_SIZE 9
 ESP8266WebServer server(80);
+DNSServer dnsServer;
+
 int relay1 = 5, relay2 = 4, relay3 = 0, relay4 = 2;
 int relay5 = 14, relay6 = 12, relay7 = 13, relay8 = 3;
 int state1 = HIGH, state2 = HIGH, state3 = HIGH, state4 = HIGH;
@@ -15,67 +16,100 @@ int state5 = HIGH, state6 = HIGH, state7 = HIGH, state8 = HIGH;
 
 const char* ApSsid = "ESP8266_8CH_Smart_Switches";
 const char* ApPassword = "ESP8266-admin";
+const byte DNS_PORT = 53;
+IPAddress apIP(192, 168, 4, 1);
+IPAddress netMsk(255, 255, 255, 0);
+
+// Captive portal handler - redirects all requests to root
+bool captivePortal() {
+  if (!isIp(server.hostHeader()) && server.hostHeader() != (String("192.168.4.1"))) {
+    server.sendHeader("Location", String("http://192.168.4.1"), true);
+    server.send(302, "text/plain", "");
+    server.client().stop();
+    return true;
+  }
+  return false;
+}
+
+bool isIp(String str) {
+  for (size_t i = 0; i < str.length(); i++) {
+    int c = str.charAt(i);
+    if (c != '.' && (c < '0' || c > '9')) {
+      return false;
+    }
+  }
+  return true;
+}
 
 void handleRoot() {
     String s = MAIN_page;
     server.send(200, "text/html", s);
 }
+
 void handlestate1() {
     handleRoot();
     state1 = !state1;
     digitalWrite(relay1, state1);
     EEPROM.write(1, state1);
     EEPROM.commit();
-  }
+}
+
 void handlestate2() {
     handleRoot();
     state2 = !state2;
     digitalWrite(relay2, state2);
     EEPROM.write(2, state2);
     EEPROM.commit();
-  }
+}
+
 void handlestate3() {
     handleRoot();
     state3 = !state3;
     digitalWrite(relay3, state3);
     EEPROM.write(3, state3);
     EEPROM.commit();
-  }
+}
+
 void handlestate4() {
     handleRoot();
     state4 = !state4;
     digitalWrite(relay4, state4);
     EEPROM.write(4, state4);
     EEPROM.commit();
-  }
+}
+
 void handlestate5() {
     handleRoot();
     state5 = !state5;
     digitalWrite(relay5, state5);
     EEPROM.write(5, state5);
     EEPROM.commit();
-  }
+}
+
 void handlestate6() {
     handleRoot();
     state6 = !state6;
     digitalWrite(relay6, state6);
     EEPROM.write(6, state6);
     EEPROM.commit();
-  }
+}
+
 void handlestate7() {
     handleRoot();
     state7 = !state7;
     digitalWrite(relay7, state7);
     EEPROM.write(7, state7);
     EEPROM.commit();
-  }
+}
+
 void handlestate8() {
     handleRoot();
     state8 = !state8;
     digitalWrite(relay8, state8);
     EEPROM.write(8, state8);
     EEPROM.commit();
-  }
+}
+
 void handleallon() {
     handleRoot();
     state1 = LOW, state2 = LOW, state3 = LOW, state4 = LOW;
@@ -90,7 +124,8 @@ void handleallon() {
     EEPROM.write(7, state7);
     EEPROM.write(8, state8);
     EEPROM.commit();
-  }
+}
+
 void handlealloff() {
     handleRoot();
     state1 = HIGH, state2 = HIGH, state3 = HIGH, state4 = HIGH;
@@ -105,7 +140,8 @@ void handlealloff() {
     EEPROM.write(7, state7);
     EEPROM.write(8, state8);
     EEPROM.commit();
-  }
+}
+
 void handlestate() {
   String content = "<?xml version = \"1.0\" ?>";
   content += "<inputs><analog>";
@@ -128,9 +164,18 @@ void handlestate() {
   server.sendHeader("Cache-Control", "no-cache");
   server.send(200, "text/xml", content);
 }
+
 void setup() {
   EEPROM.begin(EEPROM_SIZE);
+  
+  // Set up Access Point with static IP
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, netMsk);
   WiFi.softAP(ApSsid, ApPassword);
+  
+  // Start DNS server for captive portal
+  dnsServer.start(DNS_PORT, "*", apIP);
+  
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   pinMode(relay3, OUTPUT);
@@ -139,26 +184,40 @@ void setup() {
   pinMode(relay6, OUTPUT);
   pinMode(relay7, OUTPUT);
   pinMode(relay8, OUTPUT);
-  server.on("/", handleRoot); 
-  server.on("/LED1", handlestate1); 
-  server.on("/LED2", handlestate2);  
-  server.on("/LED3", handlestate3); 
-  server.on("/LED4", handlestate4);  
-  server.on("/LED5", handlestate5); 
-  server.on("/LED6", handlestate6);  
-  server.on("/LED7", handlestate7); 
-  server.on("/LED8", handlestate8);  
+  
+  // Route handlers
+  server.on("/", handleRoot);
+  server.on("/LED1", handlestate1);
+  server.on("/LED2", handlestate2);
+  server.on("/LED3", handlestate3);
+  server.on("/LED4", handlestate4);
+  server.on("/LED5", handlestate5);
+  server.on("/LED6", handlestate6);
+  server.on("/LED7", handlestate7);
+  server.on("/LED8", handlestate8);
   server.on("/allon", handleallon);
   server.on("/alloff", handlealloff);
-  server.on("/redstate", handlestate); 
+  server.on("/redstate", handlestate);
+  
+  // Captive portal handlers
+  server.onNotFound([]() {
+    if (captivePortal()) {
+      return;
+    }
+    handleRoot();
+  });
+  
   server.begin();
   getstate();
 }
+
 void loop() {
-  server.handleClient(); 
-  delay(50); 
+  dnsServer.processNextRequest();
+  server.handleClient();
+  delay(50);
 }
-void setrelaystate() { 
+
+void setrelaystate() {
     digitalWrite(relay1, state1);
     digitalWrite(relay2, state2);
     digitalWrite(relay3, state3);
@@ -168,7 +227,8 @@ void setrelaystate() {
     digitalWrite(relay7, state7);
     digitalWrite(relay8, state8);
 }
-void getstate() { 
+
+void getstate() {
     state1 = EEPROM.read(1);
     state2 = EEPROM.read(2);
     state3 = EEPROM.read(3);
